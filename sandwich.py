27c4dd992c7b4c635359c01e8200fe14878f0f88
danisung <84759289+danisung@users.noreply.github.com>
2021-05-30 14:52:02 -0700

    Add files via upload

---
+++ sandwich.py
@@ -0,0 +1,8 @@
+def make_sandwich():
+	jelly = input("What jelly would you like?")
+	butter = input("OK - and how about the butter?")
+	print("Thanks. Let's go!")
+	print("Cutting the bread...")
+	print("Smearring the" + butter)
+	print("Now adding in the " + jelly + " butter")
+	print("Finished! There's your " + butter + " and " + jelly + " sandwich!")
